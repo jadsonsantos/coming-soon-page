7970cd48628659e03541faf9475356da8d86aeb9
Jadson Santos <jadsondossantos5@gmail.com>
2020-01-25 10:57:16 -0300

    Altera estrutura do gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,30 +4,26 @@ const browserSync = require('browser-sync').create();
 
 
 function compilaSass() {
-    return gulp.src('./scss/**/*.scss')
-      .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-      .pipe(gulp.dest('./css'))
-      .pipe(browserSync.stream())
+    return gulp.src('./src/scss/**/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(gulp.dest('./dist/css/'))
+    .pipe(browserSync.stream())
 }
 
 gulp.task('sass', compilaSass);
 
-function browser() {
+gulp.task('browserSync', function() {
     browserSync.init({
-      server: {
-        baseDir: "./"
-      }
+        server: {
+            baseDir: "./"
+        }
     });
-}
+});
 
-gulp.task('browserSync', browser);
-
-function watch() {
-    gulp.watch('./scss/**/*.scss', compilaSass);
+gulp.task('watch', function() {
+    gulp.watch('./src/scss/**/*.scss', compilaSass);
     gulp.watch(['*.html']).on('change', browserSync.reload);
-    gulp.watch(['./js/**/*.js']).on('change', browserSync.reload);
-}
-
-gulp.task('watch', watch);
+    gulp.watch(['./src/js/**/*.js']).on('change', browserSync.reload);
+});
 
 gulp.task('default', gulp.parallel('watch', 'browserSync'));
